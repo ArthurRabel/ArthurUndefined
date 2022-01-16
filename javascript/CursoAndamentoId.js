bedb696cb90034fb2c795b97469b2cad5d2ddd2b
Arthur <arthur15951@gmail.com>
2022-01-15 21:31:50 -0300

    isolando info em modulo js

--- javascript/CursoAndamentoId.js
+++ javascript/CursoAndamentoId.js
@@ -1,3 +1,5 @@
+import Cursos from "./CursosAndamento.js";
+
 let organizacao = document.querySelector("#section1-titulo");
 let curso = document.querySelector("#curso");
 let detalhes = document.querySelector("#InfoDetalhes");
@@ -11,36 +13,7 @@ let skill7 = document.querySelector("#skill7");
 let skill8 = document.querySelector("#skill8");
 let img = document.querySelector("#matriculaCertificado");
 const idCurso = localStorage.getItem("idCurso");
-const Cursos = [
-    {
-        'Organizacao': 'Instituto Federal de Brasilia',
-        'Curso' : 'Tecnico de informática',
-        'Detalhes' : 'Tecnico integrado ao ensino médio 2020-2023, atualmente no 2°ano',
-        'Skill1' : '-Desenvolvimento web (Javascript e html)',
-        'Skill2' : '-Desenvolvimento desktop (javascript)',
-        'Skill3' : '-Sistema operacionais',
-        'Skill4' : '-Banco de dados',
-        'Skill5' : '-basico de engenharia de software',
-        'Skill6' : '-Logica de programação',
-        'Skill7' : '-Segurança da Informação',
-        'Skill8' : '-Web Designer',
-        'Image' : '../public/matricula.png'
-    },
-    {
-        'Organizacao': 'Udemy',
-        'Curso' : 'front-end e back-end',
-        'Detalhes' : 'estudo em 20% do curso',
-        'Skill1' : '-Javascript',
-        'Skill2' : '-Typescript ',
-        'Skill3' : '-Node.js, Frameworks e bibliotecas',
-        'Skill4' : '-NoSql',
-        'Skill5' : '-React',
-        'Skill6' : '-Design Patterns',
-        'Skill7' : '-HTML5 e CSS3 ',
-        'Skill8' : '',
-        'Image' : '../public/error.png',
-    }
-]
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
     organizacao.innerText = Cursos[idCurso].Organizacao
